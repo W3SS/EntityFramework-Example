5b6ee8d83f08ef80c157221e7fb1c55e7311fa84
Doru Cioclea <cioclea.doru@gmail.com>
2017-07-18 14:50:39 +0300

    added base EF configuration

---
+++ EfTUtorial/EfTUtorial/Database/Configurations/Base/BaseConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using EfTUtorial.Utils;
+
+namespace EfTUtorial.Database.Configurations.Base
+{
+    public class BaseConfiguration<T> : EntityTypeConfiguration<T> where T : BaseEntity
+    {
+        protected BaseConfiguration()
+        {
+            HasKey(x => x.Id);
+        }
+    }
+}
