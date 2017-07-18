53793b1d12ae8dff8937bbf3b2e678c1215f489a
Doru Cioclea <cioclea.doru@gmail.com>
2017-07-18 15:20:04 +0300

    Added autofac configuration and database module which just registers our context. in production it would register more.

---
+++ EfTUtorial/EfTUtorial/Autofac/AutofacConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using EfTUtorial.Autofac.Modules;
+using EfTUtorial.Utils;
+using EfTUtorial.Utils.Contracts;
+
+namespace EfTUtorial.Autofac
+{
+    public static class AutofacConfiguration
+    {
+        public static IContainer Container { get; private set; }
+        public static void RegisterContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.Register<IConfiguration>(c => new Configuration()).SingleInstance();
+
+            // register our database module
+
+            builder.RegisterModule(new DalModule());    
+            // register resolver
+            Container = builder.Build();
+        }
+    }
+}
---
+++ EfTUtorial/EfTUtorial/Autofac/Modules/DalModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using EfTUtorial.Database;
+using EfTUtorial.Database.Contracts;
+
+namespace EfTUtorial.Autofac.Modules
+{
+    public class DalModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<DatabaseContext>().As<IDatabaseContext>().InstancePerLifetimeScope();
+        }
+    }
+}
